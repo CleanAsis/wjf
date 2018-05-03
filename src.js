9662011b6bb452764175de46303ae75a225b3aa7
CleanAsis <>
2018-05-02 22:45:18 -0700

    Basic React Dataview

--- src.js
+++ src.js
@@ -1,6 +1,18 @@
+const fetchData = async () => await(await fetch('data.json')).json()
 
 class Viewport extends React.Component {
     render() {
-        return <h1>Hello, VIEWPORT!</h1>;
+        let dom = [];
+        dom.push(<h1 key="HV">Hello Viewport</h1>);
+        this.props.data.array.forEach(v => 
+            dom.push(<Card key={v} text={v}/>)
+        );
+        return dom;
+    }
+}
+
+class Card extends React.Component {
+    render() {
+        return <div>CARD for {this.props.text}</div>;
     }
 }
